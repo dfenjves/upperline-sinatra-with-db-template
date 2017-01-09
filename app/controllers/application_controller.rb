ef641204375b8ab3ca91a3ff7791c2a0001b9dec
dfenjves <fenjves.daniel@gmail.com>
2017-01-09 13:01:04 -0500

    fully restful routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,37 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/items' do
-    Item.create(name: params[:name], color:params[:color])
-    redirect to '/items'
+    @item = Item.create(name: params[:name], color:params[:color])
+    @error = false
+    if @item.valid?
+      @error = true
+      erb :new_item
+    else
+      redirect to '/items/new'
+    end
   end
 
   get "/items/:id" do
     @item = Item.find(params[:id])
     erb :show_item
   end
+
+  get "/items/:id/edit" do
+    @item = Item.find(params[:id])
+    erb :edit_item
+  end
+
+  post "/items/:id" do
+    @item = Item.find(params[:id])
+    @item.update(name: params[:name], color:params[:color])
+    redirect to "/items/#{params[:id]}"
+  end
+
+  get "/items/:id/delete" do
+    @item = Item.find(params[:id])
+    @item.delete
+    redirect to '/items'
+  end
+
 
 end
